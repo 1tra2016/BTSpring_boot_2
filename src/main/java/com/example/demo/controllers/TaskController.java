2680cccd0e5b55b9e6f93b09144eb27fb812b2d9
Chè <22010087@st.phenikaa-uni.edu.vn>
2026-02-05 14:50:57 +0700

    update taskCOntroller

--- src/main/java/com/example/demo/controllers/TaskController.java
+++ src/main/java/com/example/demo/controllers/TaskController.java
@@ -5,8 +5,10 @@ import com.example.demo.repositories.TaskRepository;
 import com.example.demo.services.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,7 +25,13 @@ public class TaskController {
     }
 
     @GetMapping
-    public List<Task> getAllTasks() {
-        return taskService.findAllTasks();
+    public ResponseEntity<List<Task>> getAllTasks(@RequestParam(required = false) String search) {
+        List<Task> tasks = taskService.findAllTasks();
+        if (search != null && !search.isEmpty()) {
+            tasks = tasks.stream()
+                    .filter(u -> u.getTitle().toLowerCase().contains(search.toLowerCase()))
+                    .toList();
+        }
+        return ResponseEntity.ok(tasks);
     }
 }
